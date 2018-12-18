1a2d60df4a56bdda4299ca05c3d077ec021c813a
Aniruddha Raje <aniruddha.raje@blazeclan.com>
2018-12-18 16:39:03 +0530

    [STABLE]Modified codebase

--- src/basics/dataStructure/EnumerationDemo.java
+++ src/basics/dataStructure/EnumerationDemo.java
@@ -9,20 +9,19 @@ import java.util.Vector;
  */
 public class EnumerationDemo {
 	public static void main(String args[]) {
-	      Enumeration days;
-	      Vector dayNames = new Vector();
-	      
-	      dayNames.add("Sunday");
-	      dayNames.add("Monday");
-	      dayNames.add("Tuesday");
-	      dayNames.add("Wednesday");
-	      dayNames.add("Thursday");
-	      dayNames.add("Friday");
-	      dayNames.add("Saturday");
-	      days = dayNames.elements();
-	      
-	      while (days.hasMoreElements()) {
-	         System.out.println(days.nextElement()); 
-	      }
-	   }
+
+		Vector<String> dayNames = new Vector<>();
+		dayNames.add("Sunday");
+		dayNames.add("Monday");
+		dayNames.add("Tuesday");
+		dayNames.add("Wednesday");
+		dayNames.add("Thursday");
+		dayNames.add("Friday");
+		dayNames.add("Saturday");
+		Enumeration<String> days = dayNames.elements();
+
+		while (days.hasMoreElements()) {
+			System.out.println(days.nextElement());
+		}
+	}
 }
