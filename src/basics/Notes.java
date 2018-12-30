476c0608392eb6016a274aa99a784d1f9b4d7e67
Aniruddha Raje <aniruddha.raje@blazeclan.com>
2018-12-30 19:08:41 +0530

    [STABLE]Notes edited

--- src/basics/Notes.java
+++ src/basics/Notes.java
@@ -9,6 +9,9 @@ package basics;
  */
 //JVM(Has interpreter - line by line) + Libraries = JRE
 //JRE + Development tools(Compiler) = JDK
+
+//Heap - Objects + String Pool - Younger Memory has Minor gc, Older Memory has Major gc
+//Stack - Methods and Local Variables, References
 public class Notes {
 	public static void main(String[] args) {
 		
